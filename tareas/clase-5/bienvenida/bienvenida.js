37abdda5a3c203a9c4634d1364cdff1cbd18c139
Agustin Ardizzone <AgusArdizzone@users.noreply.github.com>
2020-02-23 16:12:10 -0300

    did some tasks of class 7
    Fabricio told us to do some validation in the functions of older classes

--- tareas/clase-5/bienvenida/bienvenida.js
+++ tareas/clase-5/bienvenida/bienvenida.js
@@ -4,6 +4,33 @@ const $botonIngresar = document.querySelector('#dar-bienvenida');
 
 $botonIngresar.onclick = function(){
     const nombreUser = document.querySelector('#nombre-user').value;
-    document.querySelector('#saludo').innerText = `Hola ${nombreUser}!`
+    if(validarNombre(nombreUser)){
+        alert(validarNombre(nombreUser));
+    } else {
+        document.querySelector('#saludo').innerText = `Hola ${nombreUser}!`
+    }
+    
     return false;
 }
+
+function validarNombre(nombre){
+    if (!(/^[a-z]+$/i.test(nombre))){
+        return "El nombre debe contener al menos una letra";
+    }
+    return "";
+}
+
+console.assert(
+    validarNombre("") === "El nombre debe contener al menos una letra",
+    "Validar Nombre no valido un nombre vacio",
+)
+
+console.assert(
+    validarNombre("1234") === "El nombre debe contener al menos una letra",
+    "ValidarNombre no valido que no tenga numeros",
+)
+
+console.assert(
+    validarNombre("Agustin") === "",
+    "ValidarNombre no funciono con un nombre valido",
+)
